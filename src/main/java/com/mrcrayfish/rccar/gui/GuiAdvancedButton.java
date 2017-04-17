3d8b8965a9d04bcac9c7c078d9a200b1d435fff1
MrCrayfish <mrcrayfish@hotmail.com>
2017-04-17 23:53:39 +0930

    Added icon support for advanced buttons

--- src/main/java/com/mrcrayfish/rccar/gui/GuiAdvancedButton.java
+++ src/main/java/com/mrcrayfish/rccar/gui/GuiAdvancedButton.java
@@ -4,9 +4,13 @@ import com.mrcrayfish.rccar.gui.interfaces.ClickListener;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
+import net.minecraft.util.ResourceLocation;
 
 public class GuiAdvancedButton extends GuiButton
 {
+	private ResourceLocation icon;
+	private int iconX, iconY;
+	
 	private ClickListener listener;
 	
 	public GuiAdvancedButton(int x, int y, String buttonText) 
@@ -19,6 +23,26 @@ public class GuiAdvancedButton extends GuiButton
 		super(0, x, y, widthIn, heightIn, buttonText);
 	}
 	
+	public GuiAdvancedButton(int x, int y, ResourceLocation icon, int iconX, int iconY) 
+	{
+		super(0, x, y, 20, 20, "");
+		this.icon = icon;
+		this.iconX = iconX;
+		this.iconY = iconY;
+	}
+	
+	@Override
+	public void drawButton(Minecraft mc, int mouseX, int mouseY) 
+	{
+		super.drawButton(mc, mouseX, mouseY);
+		if(icon != null)
+		{
+			int state = getHoverState(this.hovered);
+			mc.getTextureManager().bindTexture(icon);
+			this.drawTexturedModalRect(this.xPosition + 4, this.yPosition + 4, iconX, iconY + 12 * state, 12, 12);
+		}
+	}
+
 	@Override
 	public boolean mousePressed(Minecraft mc, int mouseX, int mouseY) 
 	{
